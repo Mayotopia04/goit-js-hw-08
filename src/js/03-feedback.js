2d81cc625eb1dda96c40cd011d081ab5b735b838
Mayonaise04 <134679840+Mayonaise04@users.noreply.github.com>
2023-11-26 23:52:03 +0800

    Update 03-feedback.js
    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+const updateLocalStorageThrottled = throttle (() => {
+    const email = form.el.email.value;
+    const message = form.el.message.value;
+    const data = {email, message};
+    localStorage.setItem(localStorageKey, JSON.stringify(data));}, 500);
+
+    form.addEventListener('input', () => {
+        updateLocalStorageThrottled();
+    });
+
+    form.el.email.value = localStorage.getItem(localStorageKey) ?? "";
+    form.el.message.value = localStorage.getItem(localStorageKey) ?? "";
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const storedData = localStorage.getItem(localStorageKey);
+        if (storedData) {
+            try {
+                const { email, message} = JSON.parse(storedData);
+                console.log("Feedback submitted:");
+                console.log("Email:", email);
+                console.log("Message:", message);
+
+            } catch (error) {
+                console.error('Error parsing stored data:', error);
+            }
+        }
+        localStorage.removeItem(localStorageKey);
+        form.reset();
+    })
+
+    
+
 
